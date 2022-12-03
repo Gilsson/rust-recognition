a893b13be6cbda0c42fb18f56e27e4ca202be070
Gilsson <86572717+Gilsson@users.noreply.github.com>
2022-12-03 14:46:32 +0300

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,7 @@ pub mod network;
 
 use crate::network::layers;
 use crate::network::layers::layers_structure::Layer;
-<<<<<<< HEAD
-<<<<<<< HEAD
 use crate::network::learning::Learning;
-=======
->>>>>>> 2b34d59 (first commit)
-=======
-use crate::network::learning::Learning;
->>>>>>> 6553346 (backpropagation calculus starting)
 use image::{
     open, DynamicImage, GenericImage, GenericImageView, GrayImage, ImageBuffer, Luma, Rgb, RgbImage,
 };
@@ -18,22 +11,12 @@ use libm::fabsf;
 use std::borrow::{Borrow, BorrowMut};
 
 fn main() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 6553346 (backpropagation calculus starting)
     let mut network = Learning::new(vec![784, 16, 16]);
     println!("{:?}", network);
     for i in 0..100 {
         network.backpropagation_calculus();
     }
     /*let mut layer = LayerBias::new(16);
-<<<<<<< HEAD
-=======
-    let mut layer = LayerBias::new(16);
->>>>>>> 2b34d59 (first commit)
-=======
->>>>>>> 6553346 (backpropagation calculus starting)
     let x = 28usize;
     let y = 28usize;
     let path = "20.jpg";
@@ -41,20 +24,9 @@ fn main() {
     let mut input: Vec<f64> = Vec::new();
     check_image_field(&mut bwimage, &mut input, (0, 0));
     layer.fill_input(input);
-<<<<<<< HEAD
-<<<<<<< HEAD
     layer.sigmoid();
     println!("{:?}", layer);
     let mut layer = Sigmoid(Box::new());
-=======
-    println!("{:?}", layer);
-    /*let mut layer = Sigmoid(Box::new());
->>>>>>> 2b34d59 (first commit)
-=======
-    layer.sigmoid();
-    println!("{:?}", layer);
-    let mut layer = Sigmoid(Box::new());
->>>>>>> 6553346 (backpropagation calculus starting)
     let mut layer_sigmoid: Layers = Layers::new(Layers::LayerSigmoid { Def }, 15);
     layer_sigmoid.fill_elements(&input);
     let mut digit_layer = LayerSigmod::create_layer(&layer_sigmoid);
@@ -71,15 +43,7 @@ fn main() {
      */
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 /*fn check_image_field(image: &mut GrayImage, scale: &mut Vec<f64>, dimensions: (u32, u32)) {
-=======
-fn check_image_field(image: &mut GrayImage, scale: &mut Vec<f64>, dimensions: (u32, u32)) {
->>>>>>> 2b34d59 (first commit)
-=======
-/*fn check_image_field(image: &mut GrayImage, scale: &mut Vec<f64>, dimensions: (u32, u32)) {
->>>>>>> 6553346 (backpropagation calculus starting)
     if dimensions.1 >= image.width() {
         return;
     }
@@ -101,15 +65,7 @@ fn check_image_field(image: &mut GrayImage, scale: &mut Vec<f64>, dimensions: (u
         check_image_field(image, scale, (0, dimensions.1 + image.width() / 7));
     }
 }
-<<<<<<< HEAD
-<<<<<<< HEAD
 */
-=======
-
->>>>>>> 2b34d59 (first commit)
-=======
-*/
->>>>>>> 6553346 (backpropagation calculus starting)
 //#[test]
 //fn test_digit() {
 //    let layer = LayerDigits {
--- src/network.rs
+++ src/network.rs
@@ -1,16 +1,11 @@
 pub mod layers;
 
 pub mod learning {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 6553346 (backpropagation calculus starting)
     use crate::network::layers::layers_structure::{Layer, LayerBias, LayerDigit, LayerInput};
     use image::{open, DynamicImage, GrayImage, Luma};
     use libm::fabsf;
     use std::iter::from_fn;
     use std::rc::Rc;
-<<<<<<< HEAD
 
     #[derive(Debug)]
     pub struct Learning {
@@ -43,7 +38,7 @@ pub mod learning {
                 Learning::check_image_field(image, scale, (0, dimensions.1 + image.width() / 7));
             }
         }
-        
+
         pub fn new(layers: Vec<usize>) -> Vec<Learning> {
             let mut path = Vec::<String>::new();
             path = from_fn(|| Some(String::new()))
@@ -89,91 +84,5 @@ pub mod learning {
                 .collect::<Vec<Learning>>()
         }
         pub fn backpropagation_calculus(&mut self) {}
-=======
-    use rand::Rng;
-=======
->>>>>>> 6553346 (backpropagation calculus starting)
-
-    #[derive(Debug)]
-    pub struct Learning {
-        image: GrayImage,
-        layer_input: Rc<LayerInput>,
-        layer_bias: Rc<Vec<LayerBias>>,
-        layer_digit: Rc<LayerDigit>,
-    }
-
-    impl Learning {
-        fn check_image_field(image: &mut GrayImage, scale: &mut Vec<f64>, dimensions: (u32, u32)) {
-            if dimensions.1 >= image.width() {
-                return;
-            }
-            let y: u32 = dimensions.1;
-            for x in dimensions.0..image.width() {
-                //let pixel = Luma([rng.gen::<u8>() as u8]);
-                //image.put_pixel(x, y, pixel);
-                scale.push(fabsf(
-                    ((*image.get_pixel(x, y).0.first().unwrap()) as f32 / 255.0 - 1.0) as f32,
-                ) as f64);
-            }
-            if dimensions.0 < image.height() - image.width() / 7 {
-                Learning::check_image_field(
-                    image,
-                    scale,
-                    (dimensions.0 + image.width() / 7, dimensions.1),
-                );
-            } else {
-                Learning::check_image_field(image, scale, (0, dimensions.1 + image.width() / 7));
-            }
-        }
-        
-<<<<<<< HEAD
->>>>>>> 2b34d59 (first commit)
-=======
-        pub fn new(layers: Vec<usize>) -> Vec<Learning> {
-            let mut path = Vec::<String>::new();
-            path = from_fn(|| Some(String::new()))
-                .enumerate()
-                .map(|mut x| {
-                    x.1 = x.0.to_string() + ".jpg";
-                    x.1
-                })
-                .take(5)
-                .collect();
-            //let path = "{i}.jpg";
-            let mut image: Vec<GrayImage> = path
-                .iter()
-                .map(|x| open(x).unwrap().to_luma8())
-                .collect::<Vec<GrayImage>>();
-            let mut input: Vec<f64> = Vec::new();
-            let mut iter = layers.iter();
-            image
-                .into_iter()
-                .map(|mut x| {
-                    Learning::check_image_field(&mut x, &mut input, (0, 0));
-                    x
-                })
-                .take(5)
-                .collect::<Vec<GrayImage>>();
-            image
-                .into_iter()
-                .map(|x| Learning {
-                    image: x,
-                    layer_input: Rc::new(LayerInput::new(input)),
-                    layer_bias: Rc::new({
-                        from_fn(|| {
-                            Some(LayerBias::new(
-                                *layers.first().unwrap(),
-                                *iter.next().unwrap(),
-                            ))
-                        })
-                        .take(layers.len())
-                        .collect()
-                    }),
-                    layer_digit: Rc::new(LayerDigit::new(*layers.last().unwrap(), 10)),
-                })
-                .collect::<Vec<Learning>>()
-        }
-        pub fn backpropagation_calculus(&mut self) {}
->>>>>>> 6553346 (backpropagation calculus starting)
     }
 }
